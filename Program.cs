2dd8f9af8bb66409804dbe4c43533dccff9c7942
Wouter Van der Auwera <wvanderauwera@xpirit.com>
2023-05-15 09:51:01 +0200

    connect to db

--- Program.cs
+++ Program.cs
@@ -2,12 +2,16 @@
 using Microsoft.EntityFrameworkCore;
 
 
-namespace EfCore.Console
+namespace EfCore.Techorama
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using var db = new WouterDataBaseContext();
+            db.Database.EnsureDeleted(); // In real word scenarios use async!
+            db.Database.EnsureCreated();
+
         }
     }
 
@@ -17,12 +21,20 @@ namespace EfCore.Console
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Wouter;User Id=sa;Password=W@ffles!123;ConnectRetryCount=0;TrustServerCertificate=True");
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.EnableSensitiveDataLogging();
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Cat> Cats { get; set; }
+
     }
 
 
