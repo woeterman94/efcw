f0ec15efc54521e5813fa6f7cbc51db80fa25de8
Wouter Van der Auwera <wvanderauwera@xpirit.com>
2023-05-15 09:37:08 +0200

    dbcontext

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EfCore.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    class WouterDataBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+    class Owner
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Cat> Cats { get; set; }
+    }
+
+    class Cat
+    {
+        public int Id { get; set; }
+        public Owner Owner { get; set; }
+        public string Name { get; set; }
+    }
+}
